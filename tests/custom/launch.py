3b9234219c24c87c648e7d8cc18bc40d33bb1bcd
Flavien Chenu <flavien.chenu@epitech.eu>
2023-04-11 11:15:00 +0200

    🧪 Updated tests custom architecture

--- tests/custom/launch.py
+++ tests/custom/launch.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 from classes.tester import Tester
-import basics
+import suites
 
 if __name__ == '__main__':
     tester = Tester()
-    tester.addSuite(basics.suite)
+    tester.addSuite(suites.basics.suite)
 
     if tester.execute():
         exit(0)
