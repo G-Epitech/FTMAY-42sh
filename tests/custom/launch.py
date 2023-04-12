0828ee17336d7485379c0825a2fcdc76e6d5a304
Fradet <axel.fradet@epitech.eu>
2023-04-12 11:21:21 +0200

    ✅ Added test on semicolon

--- tests/custom/launch.py
+++ tests/custom/launch.py
@@ -2,10 +2,13 @@
 
 from classes.tester import Tester
 import suites.basics as basics
+import suites.minishell2.semicolon as minishell2Semicolon
+
 
 if __name__ == '__main__':
     tester = Tester()
     tester.addSuite(basics.suite)
+    tester.addSuite(minishell2Semicolon.suite)
 
     if tester.execute():
         exit(0)
---
+++ tests/custom/suites/minishell2/semicolon.py
@@ -0,0 +1,45 @@
+##
+## EPITECH PROJECT, 2023
+## minishell2
+## File description:
+## suite
+##
+
+import pathlib
+from classes.suite import TestSuite
+from classes.test import Test
+
+pwd = pathlib.Path(__file__).parent.resolve()
+
+global suite
+suite = TestSuite("Basics")
+
+simple_semi = Test("Simple semicolon", "echo \"ls; pwd\" | {shell}")
+suite.addTest(simple_semi)
+
+double_semi = Test("Double semicolon", "echo \"ls; echo salut; pwd\" | {shell}")
+suite.addTest(double_semi)
+
+triple_semi = Test("Triple semicolon", "echo \"ls; pwd; touch axxel; rm -rf axxel\" | {shell}")
+suite.addTest(triple_semi)
+
+wrong = Test("wrong", "echo \";\" | {shell}")
+suite.addTest(wrong)
+
+spaceWrong = Test("wrong with one space", "echo \" ;\" | {shell}")
+suite.addTest(spaceWrong)
+
+tabWrong = Test("wrong with one tab", "echo \"    ;\" | {shell}")
+suite.addTest(tabWrong)
+
+tabSpaceWrong = Test("wrong with one space and one tab", "echo \"        ;\" | {shell}")
+suite.addTest(tabSpaceWrong)
+
+SimpleWrongTab = Test("wrong with one command and tab", "echo \"ls ;      \" | {shell}")
+suite.addTest(SimpleWrongTab)
+
+SimpleWrongTabSpace = Test("Complex wrong cmd", "echo \"ls         ;      \" | {shell}")
+suite.addTest(SimpleWrongTabSpace)
+
+InversedWrong = Test("wrong inversed cmd semicolon", "echo \"; ls\" | {shell}")
+suite.addTest(InversedWrong)
