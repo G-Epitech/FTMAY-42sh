f81cfd8fa84cb3bb069fd288361230afa49ff3f7
Fradet <axel.fradet@epitech.eu>
2023-04-14 15:47:37 +0200

    ✅ Added tests about minishell2 (pipe, semicolon, ...)

--- tests/custom/launch.py
+++ tests/custom/launch.py
@@ -6,7 +6,9 @@ import suites.minishell2.semicolon as minishell2Semicolon
 import suites.minishell2.pipe as minishell2Pipe
 import suites.minishell2.simple_redirection_right as minishell2SRR
 import suites.minishell2.double_redirection_right as minishell2DRR
-
+import suites.minishell2.double_redirection_left as minishell2DRL
+import suites.minishell2.simple_redirection_left as minishell2SRL
+import suites.minishell2.complex as minishell2Complex
 
 if __name__ == '__main__':
     tester = Tester()
@@ -15,6 +17,9 @@ if __name__ == '__main__':
     tester.addSuite(minishell2Pipe.suite)
     tester.addSuite(minishell2SRR.suite)
     tester.addSuite(minishell2DRR.suite)
+    tester.addSuite(minishell2DRL.suite)
+    tester.addSuite(minishell2SRL.suite)
+    tester.addSuite(minishell2Complex.suite)
 
     if tester.execute():
         exit(0)
