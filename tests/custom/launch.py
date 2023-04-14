e9db9c35e32158028d9aff030603500507c182d2
Yann <yann.masson@epitech.eu>
2023-04-14 17:27:07 +0200

    ✅ Added cd tests

--- tests/custom/launch.py
+++ tests/custom/launch.py
@@ -2,10 +2,12 @@
 
 from classes.tester import Tester
 import suites.basics as basics
+import suites.minishell1.cd as minishell1cd
 
 if __name__ == '__main__':
     tester = Tester()
     tester.addSuite(basics.suite)
+    tester.addSuite(minishell1cd.suite)
 
     if tester.execute():
         exit(0)
