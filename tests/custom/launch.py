60ae2d113bd791deedb8c6e20986b05dfa96e7d9
Yann <yann.masson@epitech.eu>
2023-04-16 15:51:37 +0200

    🏗️ Updated to CamelCase syntax

--- tests/custom/launch.py
+++ tests/custom/launch.py
@@ -2,18 +2,18 @@
 
 from classes.tester import Tester
 import suites.basics as basics
-import suites.minishell1.cd as minishell1cd
-import suites.minishell1.env as minishell1env
-import suites.minishell1.unsetenv as minishell1unsetenv
-import suites.minishell1.setenv as minishell1setenv
+import suites.minishell1.cd as minishell1Cd
+import suites.minishell1.env as minishell1Env
+import suites.minishell1.unsetenv as minishell1Unsetenv
+import suites.minishell1.setenv as minishell1Setenv
 
 if __name__ == '__main__':
     tester = Tester()
     tester.addSuite(basics.suite)
-    tester.addSuite(minishell1cd.suite)
-    tester.addSuite(minishell1env.suite)
-    tester.addSuite(minishell1unsetenv.suite)
-    tester.addSuite(minishell1setenv.suite)
+    tester.addSuite(minishell1Cd.suite)
+    tester.addSuite(minishell1Env.suite)
+    tester.addSuite(minishell1Unsetenv.suite)
+    tester.addSuite(minishell1Setenv.suite)
 
     if tester.execute():
         exit(0)
