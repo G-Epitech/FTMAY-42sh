d625b256d248b48cb5dd5d4ae3c144eb684d8208
Flavien Chenu <flavien.chenu@epitech.eu>
2023-04-19 21:31:59 +0200

    🏷📒 Added node free function

---
+++ sources/types/node/free.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2023
+** 42sh
+** File description:
+** Node free
+*/
+
+#include "types/node/defs.h"
+
+void node_free(node_t *node, node_freeer_t freeer)
+{
+    if (!node)
+        return;
+    if (freeer)
+        freeer(node->data);
+    free(node);
+}
