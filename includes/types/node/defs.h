462eadea7578773df6149811c25c406027485a1b
Flavien Chenu <flavien.chenu@epitech.eu>
2023-04-19 20:40:00 +0200

    🏷️  Added node data type

---
+++ includes/types/node/defs.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2023
+** 42sh
+** File description:
+** Node definitions
+*/
+
+#ifndef NODE_DEFS_H_
+    #define NODE_DEFS_H_
+
+//Represent data of node_t
+typedef union u_node_data {
+    void *ptr;  //Any pointer
+} node_data_t;
+
+#endif /* !NODE_DEFS_H_ */
