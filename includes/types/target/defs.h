af446618eb94474b03d17c15c0a31d246494723f
Fradet <axel.fradet@epitech.eu>
2023-04-24 17:26:52 +0200

    🏷️  Defined target type

---
+++ includes/types/target/defs.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** 42sh
+** File description:
+** defs
+*/
+
+#ifndef TARGET_DEFS_H_
+    #define TARGET_DEFS_H_
+
+
+
+#endif /* !TARGET_DEFS_H_ */
---
+++ includes/types/target/target.h
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2023
+** 42sh
+** File description:
+** target
+*/
+
+#ifndef TARGET_H_
+    #define TARGET_H_
+
+    #include "defs.h"
+
+typedef struct s_shell shell_t;
+typedef char builtin_t(args_t *args, shell_t *shell);
+
+typedef union s_target {
+    char *path;             //Path to a binary file
+    builtin_t *builtin; 	//Pointer to a builtin function
+} target_t;
+
+#endif /* !TARGET_H_ */
