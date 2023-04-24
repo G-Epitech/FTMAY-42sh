585eadd420886cb5dbb577bef4a4e9d3dfe5a861
ThomasQUINTIN <thomas.quintin@epitech.eu>
2023-04-24 15:34:25 +0200

    ✅ Added ios valides and invalids output types unit tests

--- tests/criterion/types/ios.c
+++ tests/criterion/types/ios.c
@@ -10,7 +10,7 @@
 #include "types/io/io.h"
 #include "types/io/defs.h"
 
-Test(types_io, free_valid_ios)
+Test(types_ios, free_valid_ios)
 {
     io_t input = {NULL, IOT_DEFAULT};
     io_t output = {NULL, IOT_DEFAULT};
@@ -23,7 +23,31 @@ Test(types_io, free_valid_ios)
     cr_assert(ios.output.path == NULL);
 }
 
-Test(types_io, free_null_ios)
+Test(types_ios, free_input_valide_and_output_null_ios)
+{
+    io_t input = {NULL, IOT_DEFAULT};
+    io_t output = {NULL, IOT_DEFAULT};
+    ios_t ios = {input, output};
+
+    ios.input.path = strdup("input | empty path");
+    ios_free(&ios);
+    cr_assert(ios.input.path == NULL);
+    cr_assert(ios.output.path == NULL);
+}
+
+Test(types_ios, free_input_null_and_output_valide_ios)
+{
+    io_t input = {NULL, IOT_DEFAULT};
+    io_t output = {NULL, IOT_DEFAULT};
+    ios_t ios = {input, output};
+
+    ios.output.path = strdup("output | empty path");
+    ios_free(&ios);
+    cr_assert(ios.input.path == NULL);
+    cr_assert(ios.output.path == NULL);
+}
+
+Test(types_ios, free_null_ios)
 {
     ios_free(NULL);
 }
