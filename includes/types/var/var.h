e8b8fa12df325c652e273ef4c2a1a47252e1c187
Flavien Chenu <flavien.chenu@epitech.eu>
2023-04-25 22:14:25 +0200

    📒 Added variables list get variable

--- includes/types/var/var.h
+++ includes/types/var/var.h
@@ -2,7 +2,7 @@
 ** EPITECH PROJECT, 2023
 ** 42sh
 ** File description:
-** io
+** var
 */
 
 #ifndef VAR_H_
@@ -10,26 +10,44 @@
 
     #include <stdbool.h>
     #include "defs.h"
+    #include "types/list/defs.h"
 
 /**
- * @brief Create new var struct
- * @param name Name of struct
- * @param value Value given in parameter
+ * @brief Create new variable
+ * @param name Name of variable
+ * @param value Value to set to value
  */
 var_t *var_new(char *name, char *value);
 
 /**
  * @brief Free var_t struct with its container
- * @param variable Struct var_t to free
+ * @param variable Variable to free
  */
 void var_free(var_t *variable);
 
 /**
- * @brief Set a value in structure var_t
- * @param value Value to set in struct varuable
- * @param variable Struct var_t where set the value
- * @return True if no error else False
+ * @brief Set value of given variable
+ * @param variable Variable of which set value
+ * @param value Value to set in given variable
+ * @return true if no error else false
  */
 bool var_set_value(var_t *variable, char *value);
+
+/**
+ * @brief Get variable matching with given name
+ * @param vars List variables in which search
+ * @param name Name of variable to get
+ * @return Variable reference or NULL if not found
+ */
+var_t *var_list_get(list_t *vars, char *name);
+
+/**
+ * @brief Get value of variable matching
+ * @param vars List variables in which search
+ * @param name Name of variable of which get value
+ * @param copy Specify if value to get needs to be duplicated
+ * @return Value or NULL if not found
+ */
+char *var_list_get_value(list_t *vars, char *name, bool copy);
 
 #endif /* !VAR_H_ */
