c070704e6800f5c8317975170540767148985280
Fradet <axel.fradet@epitech.eu>
2023-04-26 13:08:58 +0200

    🏷️  Redefined name of type

--- includes/types/parsing_utils/defs.h
+++ includes/types/parsing_utils/defs.h
@@ -8,10 +8,13 @@
 #ifndef UTILS_DEFS_H_
     #define UTILS_DEFS_H_
 
+    #define INDEX_PARSING(utils) utils->index_parsing
+    #define USER_INPUT(utils) utils->input
+
 // Represent the advancement of parsing
-typedef struct s_utils {
+typedef struct s_parsing_utils {
     char *input;        // Original input given by user
     int index_parsing;  // Index of input parsed
-} utils_t;
+} parsing_utils_t;
 
 #endif /* !UTILS_DEFS_H_ */
--- sources/parsing/get_redirection.c
+++ sources/parsing/get_redirection.c
@@ -5,10 +5,12 @@
 ** parsing
 */
 
-#include "types/utils/utils.h"
+#include "types/parsing_utils/parsing_utils.h"
 #include "types/inst/inst.h"
 
-void get_redirection(utils_t utils, inst_t instruction)
+void parsing_redirection_handler(parsing_utils_t *utils, inst_t *instruction)
 {
+    (void) utils;
+    (void) instruction;
     return;
 }
