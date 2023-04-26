754dc00672715607b83e06687a93a87ca2bf39e4
Fradet <axel.fradet@epitech.eu>
2023-04-26 10:02:37 +0200

    🏷️  Added function utils in header

--- includes/types/utils/utils.h
+++ includes/types/utils/utils.h
@@ -10,5 +10,16 @@
 
     #include "defs.h"
 
+/**
+ * @brief Free a a struct utils.
+ * @param utils Utils to free
+ */
+void utils_free(utils_t utils);
+
+/**
+ * @brief Create a new utils.
+ * @return New utils struct
+ */
+utils_t *utils_new(char *input_user);
 
 #endif /* !UTILS_H_ */
---
+++ sources/types/utils/free.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2023
+** 42sh
+** File description:
+** free
+*/
+
+#include <stddef.h>
+#include "types/utils/utils.h"
+
+void utils_free(utils_t utils)
+{
+    free(utils);
+}
