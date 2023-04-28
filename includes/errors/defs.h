10dc4acbd56bd7c63b1305d91a9fbb246f4d025f
ThomasQUINTIN <thomas.quintin@epitech.eu>
2023-04-28 16:09:15 +0200

    🏷️ Added array with messages and codes for signals

--- includes/errors/defs.h
+++ includes/errors/defs.h
@@ -8,6 +8,8 @@
 #ifndef ERRORS_DEFS_H_
     #define ERRORS_DEFS_H_
 
+    #include <signal.h>
+
     #define ERRORS_SIG_MSG_LEN 31
 
 // Prototype of node structure code_msg_t
@@ -15,5 +17,39 @@ typedef struct s_args {
     int code;          //Code of error
     char *msg;         //Message corresponding to this error
 } code_msg_t;
+
+static const code_msg_t errors_sig_msg[ERRORS_SIG_MSG_LEN] = {
+    {SIGHUP, "Hangup"},
+    {SIGINT, NULL},
+    {SIGQUIT, "Quit"},
+    {SIGILL, "Illegal instruction"},
+    {SIGTRAP, "Trace/BPT trap"},
+    {SIGABRT, "Abort"},
+    {SIGBUS, "Bus error"},
+    {SIGFPE, "Floating exception"},
+    {SIGKILL, "Killed"},
+    {SIGUSR1, "User signal 1"},
+    {SIGSEGV, "Segmentation fault"},
+    {SIGUSR2, "User signal 2"},
+    {SIGPIPE, "Broken pipe"},
+    {SIGALRM, "Alarm clock"},
+    {SIGTERM, "Terminated"},
+    {SIGSTKFLT, "Stack limit exceeded"},
+    {SIGCHLD, NULL},
+    {SIGCONT, NULL},
+    {SIGSTOP, "\nSuspended (signal)"},
+    {SIGTSTP, "\nSuspended"},
+    {SIGTTIN, "\nSuspended (tty input)"},
+    {SIGTTOU, "\nSuspended (tty output)"},
+    {SIGURG, NULL},
+    {SIGXCPU, "Cputime limit exceeded"},
+    {SIGXFSZ, "Filesize limit exceeded"},
+    {SIGVTALRM, "Virtual time alarm"},
+    {SIGPROF, "Profiling time alarm"},
+    {SIGWINCH, NULL},
+    {SIGIO, "Pollable event occurred"},
+    {SIGPWR, "Power failure"},
+    {SIGSYS, "Bad system call"},
+};
 
 #endif /* !ERRORS_DEFS_H_ */
