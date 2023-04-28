dbd572133933b366222e0e8a388769e9f1568140
ThomasQUINTIN <thomas.quintin@epitech.eu>
2023-04-28 17:04:09 +0200

    📑 Added errors_get_message_of_code

---
+++ sources/errors/get.c
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2023
+** G-42sh-1
+** File description:
+** get
+*/
+
+#include <stdio.h>
+#include "errors/defs.h"
+
+char *errors_get_message_of_code(code_msg_t *messages, size_t messages_size,
+int code)
+{
+    (void) messages;
+    (void) messages_size;
+    (void) code;
+    return NULL;
+}
