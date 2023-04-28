55f8be9d547443d55fa575c2f24c059e67026fec
Flavien Chenu <flavien.chenu@epitech.eu>
2023-04-28 08:50:27 +0200

    🫐  Fixed F1 - GC-Style error

--- includes/utils/malloc2.h
+++ includes/utils/malloc2.h
@@ -17,7 +17,7 @@
     #define MALLOC2_MODE_NORMAL 1
 
 /**
- * @brief Set failing mode of malloc2 function
+ * @brief Set failing mode of malloc2 function.
  * @param set Specify if required action is set or get mode
  * @param new_mode New mode to set to malloc2 function
  * @return Current mode of malloc2
@@ -25,7 +25,7 @@
 int malloc2_mode(int action, int new_mode);
 
 /**
- * @brief Alias of malloc function with failure control
+ * @brief Alias of malloc function with failure control.
  * @param size Size to allocate
  * @return Allocated memory zone
  */
