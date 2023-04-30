3c818f146166464bd5992bc2c04d6e1f4fe239de
ThomasQUINTIN <thomas.quintin@epitech.eu>
2023-04-30 20:07:31 +0200

    📑 Added verification if the messages is NULL

--- sources/errors/get.c
+++ sources/errors/get.c
@@ -11,6 +11,8 @@
 char *errors_get_message_of_code(code_msg_t *messages, size_t messages_size,
 int code)
 {
+    if (!messages)
+        return NULL;
     for (size_t i = 0; i < messages_size; i++) {
         if (messages[i].code == code)
             return messages[i].msg;
