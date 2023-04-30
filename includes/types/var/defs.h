bd039e81042c1c067ab9ca25953a9af5b4ec6a69
Flavien Chenu <flavien.chenu@epitech.eu>
2023-04-30 16:00:54 +0200

    🏷️  Added prototypes of getter and setter of special variables

--- includes/types/var/defs.h
+++ includes/types/var/defs.h
@@ -8,6 +8,14 @@
 #ifndef VAR_DEFS_H_
     #define VAR_DEFS_H_
 
+typedef struct s_shell shell_t;
+
+// Prototype of special variable setter
+typedef bool (*var_special_setter_t)(char *name, shell_t *shell);
+
+// Prototype of special variable getter
+typedef char *(*var_special_getter_t)(char *name, shell_t *shell);
+
 // Represent a variable
 typedef struct s_var {
     char *name;     //Name of variable
