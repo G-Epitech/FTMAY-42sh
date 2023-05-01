71f2cc99f1dcfcf3744949150b0dc319c27d103f
Fradet <axel.fradet@epitech.eu>
2023-05-01 19:57:56 +0200

    🔀 Merged create main parsing to parsing function

---
+++ sources/parsing/separator.c
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2023
+** 42sh
+** File description:
+** separator
+*/
+
+#include <unistd.h>
+#include "types/inst/inst.h"
+#include "types/parsing_utils/parsing_utils.h"
+
+bool parsing_separator_handler(parsing_utils_t *utils, inst_t *instruction)
+{
+    char *separator[2] = {"||", "&&"};
+    char *data = utils->input + utils->index_parsing;
+
+    
+
+
+}
