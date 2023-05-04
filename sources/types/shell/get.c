3ac1a4c2fae45fbedc65a2036a518ce904841936
ThomasQUINTIN <thomas.quintin@epitech.eu>
2023-05-04 09:53:13 +0200

    📑 Added prompt before the user input

--- sources/types/shell/get.c
+++ sources/types/shell/get.c
@@ -9,12 +9,18 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+static void prompt_display(void)
+{
+    printf("> ");
+}
+
 char *shell_get_input(void)
 {
     char *input_user = NULL;
     size_t len = 0;
     int out = 0;
 
+    prompt_display();
     while (out <= 0)
         out = getline(&input_user, &len, stdin);
     return input_user;
