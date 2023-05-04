7b9b72c5bdf71658649fd4cc7836eb8bb15b5531
Fradet <axel.fradet@epitech.eu>
2023-05-04 19:13:24 +0200

    🏷️  Documented struct in header

--- includes/parsing/defs.h
+++ includes/parsing/defs.h
@@ -15,8 +15,8 @@
 
 // Struct of limit of a word.
 typedef struct s_index_word {
-    int start;
-    int end;
+    int start;      // Start index of the word
+    int end;        // End index of the word
 } index_word_t;
 
 #endif /* !PARSING_DEFS_H_ */
