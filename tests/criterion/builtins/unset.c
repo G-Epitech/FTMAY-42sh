beb82784213d1e87984044a518accf210c0987d9
TekMath <dokuhebi.game@gmail.com>
2023-05-05 11:11:50 +0200

    ✅ Added unset builtin tests

---
+++ tests/criterion/builtins/unset.c
@@ -0,0 +1,60 @@
+/*
+** EPITECH PROJECT, 2023
+** 42sh
+** File description:
+** exit
+*/
+
+#include <stdlib.h>
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+#include "builtins/builtins.h"
+#include "types/shell/shell.h"
+
+Test(builtins_unset, unvalid_unset, .init=cr_redirect_stderr) {
+    char *argv[] = {"unsetenv"};
+    args_t args = {.argc = 1, .argv = argv};
+    shell_t *shell = shell_new();
+
+    builtin_unset(&args, shell);
+    cr_assert_stderr_eq_str("unset: Too few arguments.\n");
+    shell_free(shell);
+}
+
+Test(builtins_unset, simple_unsetenv) {
+    char *argv[] = {"unsetenv", "super"};
+    args_t args = {.argc = 2, .argv = argv};
+    char *argv_set[] = {"set", "super"};
+    args_t args_set = {.argc = 2, .argv = argv_set};
+    shell_t *shell = shell_new();
+
+    builtin_set(&args_set, shell);
+    cr_assert_str_eq(NODE_DATA_TO_PTR(shell->vars->last->data, var_t *)->name, "super");
+    cr_assert_str_eq(NODE_DATA_TO_PTR(shell->vars->last->data, var_t *)->value, "");
+    int len = shell->vars->len;
+    builtin_unset(&args, shell);
+    shell->vars->len = len - 1;
+    shell_free(shell);
+}
+
+Test(builtins_unset, multi_unsetenv) {
+    char *argv[] = {"unsetenv", "super", "cool"};
+    args_t args = {.argc = 3, .argv = argv};
+    char *argv_set[] = {"set", "super", "test", "cool"};
+    args_t args_set = {.argc = 4, .argv = argv_set};
+    shell_t *shell = shell_new();
+
+    builtin_set(&args_set, shell);
+    cr_assert_str_eq(NODE_DATA_TO_PTR(shell->vars->last->data, var_t *)->name, "cool");
+    cr_assert_str_eq(NODE_DATA_TO_PTR(shell->vars->last->data, var_t *)->value, "");
+    cr_assert_str_eq(NODE_DATA_TO_PTR(shell->vars->last->prev->data, var_t *)->name, "test");
+    cr_assert_str_eq(NODE_DATA_TO_PTR(shell->vars->last->prev->data, var_t *)->value, "");
+    cr_assert_str_eq(NODE_DATA_TO_PTR(shell->vars->last->prev->prev->data, var_t *)->name, "super");
+    cr_assert_str_eq(NODE_DATA_TO_PTR(shell->vars->last->prev->prev->data, var_t *)->value, "");
+    int len = shell->vars->len;
+    builtin_unset(&args, shell);
+    shell->vars->len = len - 2;
+    cr_assert_str_eq(NODE_DATA_TO_PTR(shell->vars->last->data, var_t *)->name, "test");
+    cr_assert_str_eq(NODE_DATA_TO_PTR(shell->vars->last->data, var_t *)->value, "");
+    shell_free(shell);
+}
