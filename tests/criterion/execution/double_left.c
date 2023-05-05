f9eeeee1a6f0eb0f8a56fec480cb8b94326a9d2d
Yann <yann.masson@epitech.eu>
2023-05-05 11:15:43 +0200

    ✅ Added tests for double left rediretion

--- tests/criterion/execution/double_left.c
+++ tests/criterion/execution/double_left.c
@@ -48,6 +48,31 @@ Test(double_redirection_left, test_pipe_input, .init=redirect_all_stdout) {
     close(p[0]);
 }
 
+Test(double_redirection_left, test_word_without_breakline, .init=redirect_all_stdout) {
+    char* test_input = "0123456789\nend";
+    char reader[100];
+    int p[2] = {0, 0};
+    int fd[2] = {0, 0};
+
+    memset(reader, '\0', 100);
+    cr_assert(pipe(p) != -1, "Failed to create pipe.");
+
+    cr_assert(dup2(p[0], STDIN_FILENO) != -1, "Failed to redirect stdin to pipe.");
+
+    write(p[1], test_input, strlen(test_input));
+    close(p[1]);
+
+    pipe(fd);
+    execution_double_left("end", fd);
+
+    read(fd[0], reader, 100);
+    cr_assert(strcmp(reader, "0123456789\n") == 0, "Wrong Output");
+
+    close(fd[1]);
+    close(fd[0]);
+    close(p[0]);
+}
+
 Test(double_redirection_left, test_hard_exit, .init=redirect_all_stdout) {
     char* test_input = "0123456789\n\0";
     char reader[100];
