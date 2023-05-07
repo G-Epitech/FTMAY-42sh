230b7b481a45d3c3cb93a2c291d21316391593ae
ThomasQUINTIN <thomas.quintin@epitech.eu>
2023-05-07 14:13:25 +0200

    ✅ Added test for the function shell_get_input ( simple and command input )

--- tests/criterion/types/shell/shell.c
+++ tests/criterion/types/shell/shell.c
@@ -69,3 +69,43 @@ Test(exit_shell, shell_exit_default, .init=cr_redirect_stdout)
     cr_assert_eq(shell->status, SH_EXITED);
     shell_free(shell);
 }
+
+Test(input_shell, shell_input_default) {
+    char* test_input = "shell_input_default";
+    shell_t *shell = shell_new();
+    char reader[100];
+    int p[2] = {0, 0};
+    char *output = NULL;
+
+    memset(reader, '\0', 100);
+    cr_assert(pipe(p) != -1, "Failed to create pipe.");
+    cr_assert(dup2(p[0], STDIN_FILENO) != -1, "Failed to redirect stdin to pipe.");
+    write(p[1], test_input, strlen(test_input));
+    close(p[1]);
+
+    output = shell_get_input(shell);
+    cr_assert_str_eq(output, test_input);
+
+    close(p[0]);
+    shell_free(shell);
+}
+
+Test(input_shell, shell_input_command) {
+    char* test_input = "\0";
+    shell_t *shell = shell_new();
+    char reader[100];
+    int p[2] = {0, 0};
+    char *output = NULL;
+
+    memset(reader, '\0', 100);
+    cr_assert(pipe(p) != -1, "Failed to create pipe.");
+    cr_assert(dup2(p[0], STDIN_FILENO) != -1, "Failed to redirect stdin to pipe.");
+    write(p[1], test_input, strlen(test_input));
+    close(p[1]);
+
+    output = shell_get_input(shell);
+    cr_assert_str_eq(output, test_input);
+
+    close(p[0]);
+    shell_free(shell);
+}
