c608dc9104496b4b88ba09dd2d20035197ce6da4
ThomasQUINTIN <thomas.quintin@epitech.eu>
2023-05-09 19:19:54 +0200

    🏷️ Added array structure *function

--- includes/types/input_line/defs.h
+++ includes/types/input_line/defs.h
@@ -9,6 +9,9 @@
     #define INPUT_LINE_DEFS_H_
 
     #include <termios.h>
+    #include "shell/defs.h"
+
+    #define CTRL_KEY_HANDLERS_NBR 4
 
 // Represent value of arrow key
 typedef enum e_ctrl_key {
@@ -37,5 +40,22 @@ typedef struct s_input_line {
     input_line_buffer_t *buffer;     // Information about user input
     input_line_status_t status;      // Input line current status
 } input_line_t;
+
+// Prototype function control handler
+typedef void (*ctrl_handler_t)(shell_t *shell, input_line_t *line);
+
+// Prototype control key handler
+typedef struct s_ctrl_key_handler {
+    ctrl_key_t key;                 // Value of arrow key
+    ctrl_handler_t handler;         // Function arrow key
+} ctrl_key_handler_t;
+
+// Array control key handlers
+static const ctrl_key_handler_t ctrl_key_handlers[CTRL_KEY_HANDLERS_NBR] = {
+    {ARROW_KEY_UP, NULL},
+    {ARROW_KEY_DOWN, NULL},
+    {ARROW_KEY_RIGHT, NULL},
+    {ARROW_KEY_LEFT, NULL}
+};
 
 #endif /* !INPUT_LINE_DEFS_H_ */
--- sources/types/input_line/iscntrl.c
+++ sources/types/input_line/iscntrl.c
@@ -5,6 +5,7 @@
 ** iscntrl
 */
 
+#include <stdio.h>
 #include "utils/utils.h"
 #include "types/input_line/input_line.h"
 
@@ -15,6 +16,9 @@ int input_line_get_cntrl(void)
     switch (character) {
         case KEY_ARROWS:
             input_line_get_arrows();
+            break;
+        case KEY_CTRLS:
+            printf("ctrl + %c\n", character);
             break;
         default:
             break;
