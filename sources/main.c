3577930dcd02f7454d10ce3f44d7a4266d66ef60
Fradet <axel.fradet@epitech.eu>
2023-05-09 21:27:12 +0200

    📑 Added function for separate the user input at cursor position

--- sources/main.c
+++ sources/main.c
@@ -15,5 +15,17 @@ int main(void)
     setvbuf(stdin, NULL, _IONBF, 0);
     setvbuf(stdout, NULL, _IONBF, 0);
     setvbuf(stderr, NULL, _IONBF, 0);
-    return mysh();
+
+    input_line_t *user_input = input_line_new();
+    user_input->buffer->cursor = 2;
+    user_input->buffer->content = malloc(6);
+    user_input->buffer->content[0] = 'c';
+    user_input->buffer->content[1] = 'o';
+    user_input->buffer->content[2] = 'u';
+    user_input->buffer->content[3] = 'c';
+    user_input->buffer->content[4] = 'o';
+    user_input->buffer->content[5] = '\0';
+    append_char(user_input, 66);
+    return 0;
+    //return mysh();
 }
--- sources/types/input_line/append_char.c
+++ sources/types/input_line/append_char.c
@@ -8,19 +8,52 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "utils/malloc2.h"
 #include "types/input_line/input_line.h"
+
+static char *get_word(char *user_input, int start, int end)
+{
+    char *word = malloc2(sizeof(char) * end - start + 1);
+    int index = 0;
+
+    if (!word)
+        return NULL;
+    memset(word, '\0', end - start + 1);
+    while (start < end) {
+        word[index] = user_input[start];
+        index++;
+        start++;
+    }
+    return word;
+}
 
 bool append_char(input_line_t *line, int c)
 {
     char *user_input = line->buffer->content;
 
-    char *new_input = realloc(user_input, sizeof(char) * (strlen(user_input) + 2));
+    (void) c;
+    // char *new_input = realloc(user_input, sizeof(char) * (strlen(user_input) + 2));
+    // if (!new_input)
+    //     return false;
+    // new_input[strlen(new_input)] = c;
+    // new_input[strlen(new_input)] = '\0';
+
+    char *begin = get_word(user_input, 0, line->buffer->cursor);
+    char *end = get_word(user_input, line->buffer->cursor, strlen(user_input));
+
+    printf("begin : [%s] | [%s] : end\n", begin, end);
+
+    char *new_input = realloc(begin, sizeof(char) * (strlen(begin) + 2));
     if (!new_input)
         return false;
     new_input[strlen(new_input)] = c;
     new_input[strlen(new_input)] = '\0';
-    line->buffer->content = new_input;
-    line->buffer->cursor++;
-    line->buffer->len++;
+
+    
+
+    
+    // line->buffer->content = new_input;
+    // line->buffer->cursor++;
+    // line->buffer->len++;
     return true;
 }
