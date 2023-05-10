07d90ca0648143040cb73baa8578adde8575a0df
ThomasQUINTIN <thomas.quintin@epitech.eu>
2023-05-10 15:59:24 +0200

    📑 Added history switch content

--- sources/types/input_line/handlers/history.c
+++ sources/types/input_line/handlers/history.c
@@ -15,14 +15,22 @@ static void replace_buffer_content(input_line_t *line, char *str)
     int len = strlen(str);
 
     line->buffer->cursor = len;
+    line->buffer->len = len;
 }
 
 void input_line_history_prev(shell_t *shell, input_line_t *line)
 {
-    history_entry_t *selected = history_prev(shell->history);
-
+    history_entry_t *selected = history_current(shell->history);
+    
     if (!selected)
         return;
+    if (line->buffer->history_cursor == 0 && !line->buffer->history_content)
+        line->buffer->history_content = line->buffer->content;
+    if (line->buffer->history_cursor > 0)
+        selected = history_prev(shell->history);
+    if (!selected)
+        return;
+    line->buffer->history_cursor++;
     line->buffer->content = selected->input;
     replace_buffer_content(line, selected->input);
     refresh_screen(line);
@@ -32,9 +40,16 @@ void input_line_history_next(shell_t *shell, input_line_t *line)
 {
     history_entry_t *selected = history_next(shell->history);
 
+    if (line->buffer->history_cursor == 0 && line->buffer->history_content) {
+        line->buffer->content = line->buffer->history_content;
+        replace_buffer_content(line, line->buffer->content);
+        refresh_screen(line);
+        line->buffer->history_content = NULL;
+    }
     if (!selected)
         return;
     line->buffer->content = selected->input;
+    line->buffer->history_cursor--;
     replace_buffer_content(line, selected->input);
     refresh_screen(line);
 }
