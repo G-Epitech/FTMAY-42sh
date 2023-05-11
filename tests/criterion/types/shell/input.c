4b261eb7c8bd7079222516942aa7fe3d6cbafdb6
TekMath <dokuhebi.game@gmail.com>
2023-05-11 23:38:24 +0200

    🔥 Removed usless test

--- tests/criterion/types/shell/input.c
+++ tests/criterion/types/shell/input.c
@@ -37,25 +37,3 @@ Test(shell_get_input_tests, basic)
     shell_free(shell);
     free(input);
 }
-
-Test(shell_get_input_tests, exit_shell, .init=cr_redirect_stdout) {
-    char* test_input = "";
-    shell_t *shell = shell_new(builtins_cmds);
-    char reader[100];
-    int p[2] = {0, 0};
-    char *output = NULL;
-
-    shell->is_tty = true;
-    memset(reader, '\0', 100);
-    cr_assert(pipe(p) != -1, "Failed to create pipe.");
-    cr_assert(dup2(p[0], STDIN_FILENO) != -1, "Failed to redirect stdin to pipe.");
-    write(p[1], test_input, strlen(test_input));
-    close(p[1]);
-    output = shell_get_input(shell);
-    cr_assert_str_eq(output, test_input);
-    fflush(stdout);
-    cr_assert_stdout_eq_str("exit\n");
-    close(p[0]);
-    free(output);
-    shell_free(shell);
-}
