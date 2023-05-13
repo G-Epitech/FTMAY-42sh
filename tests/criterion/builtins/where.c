c759b7da1f765f4b71670ed0ea1cd302d75e3925
Fradet <axel.fradet@epitech.eu>
2023-05-13 17:12:51 +0200

    🐛 Fixed tests criterion with new name

--- tests/criterion/builtins/where.c
+++ tests/criterion/builtins/where.c
@@ -26,32 +26,32 @@
 
 Test(builtins_where_test, test_is_system, .init=cr_redirect_stdout)
 {
-    cr_assert_eq(is_system("/bin/ls"), true);
+    cr_assert_eq(builtin_where_is_system("/bin/ls"), true);
 }
 
 Test(builtins_where_test, bad_test_is_system, .init=cr_redirect_stdout)
 {
-    cr_assert_eq(is_system(NULL), false);
+    cr_assert_eq(builtin_where_is_system(NULL), false);
 }
 
 Test(builtins_where_test, bad_name_test_get_path, .init=cr_redirect_stdout)
 {
-    cr_assert_null(get_path(NULL, NULL));
+    cr_assert_null(builtin_where_get_path(NULL, NULL));
 }
 
 Test(builtins_where_test, test_get_path_ls, .init=cr_redirect_stdout)
 {
-    cr_assert_str_eq(get_path("/bin", "ls"), "/bin/ls");
+    cr_assert_str_eq(builtin_where_get_path("/bin", "ls"), "/bin/ls");
 }
 
 Test(builtins_where_test, test_get_path_ls_with_slash, .init=cr_redirect_stdout)
 {
-    cr_assert_str_eq(get_path("/bin", "/ls"), "/bin//ls");
+    cr_assert_str_eq(builtin_where_get_path("/bin", "/ls"), "/bin//ls");
 }
 
 Test(builtins_where_test, test_get_path_empty, .init=cr_redirect_stdout)
 {
-    cr_assert_str_eq(get_path("", "ls"), "/ls");
+    cr_assert_str_eq(builtin_where_get_path("", "ls"), "/ls");
 }
 
 Test(builtins_where_test, args_null, .init=cr_redirect_stdout)
