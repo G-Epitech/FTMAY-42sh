ad46928770191a1ee14fe0be50044af2a9a459b0
ThomasQUINTIN <thomas.quintin@epitech.eu>
2023-05-13 17:12:27 +0200

    📑Added manage ignoreeof in shell_exit & fixed display with the '\n'

--- sources/types/shell/exit.c
+++ sources/types/shell/exit.c
@@ -6,25 +6,32 @@
 */
 
 #include <stdio.h>
+#include <string.h>
 #include "types/shell/shell.h"
 
 static void exit_shell(shell_t *shell)
 {
-    shell->status = SH_EXITED;
-    printf("exit\n");
+    if (shell->status == SH_EXITED)
+        printf("exit\n");
+    else {
+        shell->status = SH_EXITED;
+        printf("exit");
+    }
 }
 
 void shell_exit(shell_t *shell)
 {
     char *ignoreeof = NULL;
 
-    if (shell->is_tty == true)
+    if (shell->is_tty != true)
         return exit_shell(shell);
     ignoreeof = shell_get_var(shell, "ignoreeof", false);
-    if (!ignoreeof)
+    if (ignoreeof)
+        shell->exit_in--;
+    if (!ignoreeof || shell->status == SH_EXITED)
         return exit_shell(shell);
-    else if (ignoreeof)
-        printf("Use 'exit' to leave tcsh.\n");
-    else
+    else if (shell->exit_in > 0) {
+        printf("\r\nUse \"exit\" to leave tcsh.");
+    } else
         return exit_shell(shell);
 }
---
+++ sources/types/shell/vars/special/ignoreeof.c
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2023
+** 42sh
+** File description:
+** home
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+#include <stdlib.h>
+#include "utils/utils.h"
+#include "types/var/var.h"
+#include "utils/malloc2.h"
+#include "utils/asprintf2.h"
+#include "types/shell/shell.h"
+
+bool shell_special_vars_ignoreeof_set(char *name, char *value, char *dependency,
+shell_t *shell)
+{
+    int exit_in = 0;
+
+    (void) dependency;
+    if (!is_number(value))
+        exit_in = 26;
+    else
+        exit_in = atoi(value);
+    shell->exit_in = exit_in;
+    return var_list_set(shell->vars, name, value);
+}
