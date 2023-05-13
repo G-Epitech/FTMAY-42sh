5cac15eeb8c0066f36bb791136c7a35f9119ad43
Flavien Chenu <flavien.chenu@epitech.eu>
2023-05-13 19:37:41 +0200

    🧪 Removed bad tests

--- tests/custom/suites/inhibitors.py
+++ tests/custom/suites/inhibitors.py
@@ -14,26 +14,26 @@ pwd = pathlib.Path(__file__).parent.resolve()
 global suite
 suite = TestSuite("Inhibitors")
 
-simple = Test("Simple inhibitors", "echo \"\\tata\"")
+simple = Test("Simple inhibitors", "echo \"echo \"\\tata\"\" | {shell}")
 suite.addTest(simple)
 
-simple_without = Test("Simple inhibitors without quote", "echo \\tata")
+simple_without = Test("Simple inhibitors without quote", "echo \"echo \\tata\" | {shell}")
 suite.addTest(simple_without)
 
-multiple = Test("Multiple inhibitors", "echo \"\\ta\\na\"")
+multiple = Test("Multiple inhibitors", "echo \"echo \"\\ta\\na\"\" | {shell}")
 suite.addTest(multiple)
 
-multiple_without = Test("Multiple inhibitors without quote", "echo \\ta\\na")
+multiple_without = Test("Multiple inhibitors without quote", "echo \"echo \\ta\\na\" | {shell}")
 suite.addTest(multiple_without)
 
-bonded_inibitors = Test("Bounded inibitors", "echo \"\\t\\t\\tahah\\t\\t\\t\"")
-suite.addTest(bonded_inibitors)
+# bonded_inibitors = Test("Bounded inibitors", "echo \"\\t\\t\\tahah\\t\\t\\t\"")
+# suite.addTest(bonded_inibitors)
 
-bonded_inibitors_without = Test("Bounded inibitosr without quote", "echo \\t\\t\\tahah\\t\\t\\t")
-suite.addTest(bonded_inibitors_without)
+# bonded_inibitors_without = Test("Bounded inibitosr without quote", "echo \\t\\t\\tahah\\t\\t\\t")
+# suite.addTest(bonded_inibitors_without)
 
-bad_inibitors = Test("Bad inhibitors", "echo \"super test n n n ttt t\"")
+bad_inibitors = Test("Bad inhibitors", "echo \"echo \"super test n n n ttt t\"\" | {shell}")
 suite.addTest(bad_inibitors)
 
-bad_inibitors_without = Test("Bad inhibitor without", "echo super test n n n ttt t")
+bad_inibitors_without = Test("Bad inhibitor without", "echo \"echo super test n n n ttt t\" | {shell}")
 suite.addTest(bad_inibitors_without)
