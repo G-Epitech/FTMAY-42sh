ac16aa0192d9c8f833a17ac639495d0264472403
ThomasQUINTIN <thomas.quintin@epitech.eu>
2023-05-13 15:35:45 +0200

    📑 Edited shell_exit for gestion ignoreeof

--- sources/types/shell/exit.c
+++ sources/types/shell/exit.c
@@ -8,9 +8,23 @@
 #include <stdio.h>
 #include "types/shell/shell.h"
 
-void shell_exit(shell_t *shell)
+static void exit_shell(shell_t *shell)
 {
     shell->status = SH_EXITED;
+    printf("exit\n");
+}
+
+void shell_exit(shell_t *shell)
+{
+    char *ignoreeof = NULL;
+
     if (shell->is_tty == true)
-        printf("exit\n");
+        return exit_shell(shell);
+    ignoreeof = shell_get_var(shell, "ignoreeof", false);
+    if (!ignoreeof)
+        return exit_shell(shell);
+    else if (ignoreeof)
+        printf("Use 'exit' to leave tcsh.\n");
+    else
+        return exit_shell(shell);
 }
