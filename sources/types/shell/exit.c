aa0c11d8f349c4508112e10c2817403ca94d6cdc
ThomasQUINTIN <thomas.quintin@epitech.eu>
2023-05-14 16:58:50 +0200

    📑 Edited function for used shell_exit_code

--- sources/types/shell/exit.c
+++ sources/types/shell/exit.c
@@ -7,6 +7,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include "utils/asprintf2.h"
 #include "types/shell/shell.h"
 
 static void exit_shell(shell_t *shell)
@@ -33,4 +34,14 @@ void shell_exit(shell_t *shell)
     } else {
         return exit_shell(shell);
     }
+}
+
+bool shell_set_exit_code(shell_t *shell, int code)
+{
+    char *exit_code = NULL;
+
+    shell->exit_code = code;
+    if (asprintf2(&exit_code, "%i", shell->exit_code) == -1)
+        return false;
+    return shell_set_var(shell, "status", exit_code);
 }
