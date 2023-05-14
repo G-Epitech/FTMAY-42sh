5cd46765b928e09e8f58ea6231f73834891b3274
Fradet <axel.fradet@epitech.eu>
2023-05-14 12:53:13 +0200

    ✅ Added basics tests

---
+++ tests/custom/suites/variables.py
@@ -0,0 +1,15 @@
+##
+## EPITECH PROJECT, 2023
+## minishell2
+## File description:
+## suite
+##
+
+import pathlib
+from classes.suite import TestSuite
+from classes.test import Test
+
+pwd = pathlib.Path(__file__).parent.resolve()
+
+global suite
+suite = TestSuite("Variable")
