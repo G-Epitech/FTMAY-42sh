5064950225c48ea34396190bd483af06f1ee2610
TekMath <dokuhebi.game@gmail.com>
2023-05-14 12:26:16 +0200

    ✅ Added complexe commands

--- tests/custom/suites/complexes.py
+++ tests/custom/suites/complexes.py
@@ -18,3 +18,14 @@ suite = TestSuite("Complexes")
 test = Test("Complex parentheses, redirections and conditions",
             "echo \"(false || echo in) | (ls | cat -e) > file.tmp && cat file.tmp; rm -f file.tmp\" | {shell}")
 suite.addTest(test)
+
+test = Test("Mix pipe & redirection", "echo \"(ls -R . | grep \".\" | sort) > errors.txt && rm -f errors.txt\" | {shell}")
+suite.addTest(test)
+
+test = Test("Mix multiple redirections and conditions",
+            "echo \"((ls | cat -e) && ls) > output.txt && rm -f output.txt\" | {shell}")
+suite.addTest(test)
+
+test = Test("Big complexe command",
+            "echo \"echo \"super example\" > input.loog; (((grep \"example\" < input.loog) >> output1.loog) > errors.loog) || ((ls | cat) > output2.loog) && (sort | uniq) | wc -l > final_output.loog\" | {shell}; rm -f *.loog")
+suite.addTest(test)
